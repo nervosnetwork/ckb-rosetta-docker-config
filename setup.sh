0b41e19898725b9fa9cc77d850aad13d3c355e06
shaojunda <shaojunda@gmail.com>
2020-09-21 19:22:11 +0800

    feat: support nginx and chain env

--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@
 #!/bin/bash
 set -ex
 
-[ -d /data/node ] || ckb init -c mainnet -C /data/node
+[ -d /data/node ] || ckb init -c $NETWORK -C /data/node
+
+if [ "$NGINX_READ_TIMEOUT" != "60" ]; then
+  sed -i s/@NGINX_READ_TIMEOUT/$NGINX_READ_TIMEOUT/ /conf/nginx.conf
+fi
