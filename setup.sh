fe307b6d9a3382462c36c236719c5b9b79b2713b
shaojunda <shaojunda@gmail.com>
2020-09-22 12:41:02 +0800

    chore: set network on rosetta server

--- setup.sh
+++ setup.sh
@@ -4,6 +4,5 @@ set -ex
 
 [ -d /data/node ] || ckb init -c $NETWORK -C /data/node
 
-if [ "$NGINX_READ_TIMEOUT" != "60" ]; then
-  sed -i s/@NGINX_READ_TIMEOUT/$NGINX_READ_TIMEOUT/ /conf/nginx.conf
-fi
+sed -i s/@NGINX_READ_TIMEOUT/$NGINX_READ_TIMEOUT/ /conf/nginx.conf
+sed -i s/@NETWORK/$NETWORK/ /config.yaml
